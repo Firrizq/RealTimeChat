1ec47fba9e40b4ee43b1504b8480c2b054884c05
Firrizq <firrizq@gmail.com>
2023-05-31 14:46:23 +0700

    add main, component

--- resources/views/livewire/chat/main.blade.php
+++ resources/views/livewire/chat/main.blade.php
@@ -1,3 +1,20 @@
 <div>
     {{-- Do your work, then step back. --}}
+
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Chat') }}
+        </h2>
+    </x-slot>
+
+    <div class="chat_container">
+        <div class="chat_list_container">
+            @livewire('chat.chatlist')
+        </div>
+        <div class="chat_box_container">
+            @livewire('chat.chatbox')
+            @livewire('chat.send-message')
+        </div>
+    </div>
+
 </div>
