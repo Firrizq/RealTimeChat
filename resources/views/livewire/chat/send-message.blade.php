f555babad98cd2e83833034eb7a674083bd5aa24
Firrizq <firrizq@gmail.com>
2023-06-02 10:37:44 +0700

    send message design

--- resources/views/livewire/chat/send-message.blade.php
+++ resources/views/livewire/chat/send-message.blade.php
@@ -1,6 +1,9 @@
 <div>
     {{-- Stop trying to control. --}}
     <div class="chatbox_footer">
-        footer new
+        <div class="custom_form_group">
+            <input type="text" class="control" placeholder="Write Message">
+            <button class="submit" type="submit">Send</button>
+        </div>
     </div>
 </div>
